68196456d128aa8534ad37b359e5b036d50f61ba
Felipe Guizar Diaz <felipegaiacharly@gmail.com>
2019-08-12 16:07:21 -0500

    Add methods to render component in the browser using id

--- src/index.js
+++ src/index.js
@@ -1,24 +1,47 @@
 
-import hypernova, { serialize, load } from 'hypernova';
+import hypernova, { serialize, load, fromScript } from 'hypernova';
 import { mount } from 'redom';
+
+const mountComponent = (Component, node, data) => {
+  const { firstChild } = node;
+  const obj = new Component(data);
+
+  if (firstChild) {
+    mount(node, obj, firstChild);
+    return node.removeChild(firstChild);
+  }
+  return mount(node, obj);
+};
+
+export const renderClient = (name, Component, id) => {
+  const key = name.replace(/\W/g, '');
+  const node = global.document.querySelector(`div[data-hypernova-key="${String(key)}"][data-hypernova-id="${String(id)}"]`);
+
+  if (node) {
+    const data = fromScript({
+      'hypernova-key': key,
+      'hypernova-id': id,
+    });
+
+    mountComponent(Component, node, data);
+  }
+};
 
 export const renderRedom = (name, Component) => hypernova({
   server() {
-    const domino = require('domino');
-    const domImpl = domino.createDOMImplementation()
-
     return async (props) => {
       let newProps = props;
 
       if (typeof Component.serverPrefetch === 'function') {
         newProps = await Component.serverPrefetch(props);
       }
-
+      const domino = require('domino'); // eslint-disable-line global-require
+      const domImpl = domino.createDOMImplementation();
       global.document = domImpl.createHTMLDocument();
-      global.SVGElement = function SVGElement () {};
+      global.SVGElement = function SVGElement() {};
 
-      mount(document.body, new Component(newProps));
-      return serialize(name, document.body.innerHTML, newProps);
+      mount(global.document.body, new Component(newProps));
+      return serialize(name, global.document.body.innerHTML, newProps);
     };
   },
 
@@ -27,15 +50,7 @@ export const renderRedom = (name, Component) => hypernova({
     if (payloads) {
       payloads.forEach((payload) => {
         const { node, data } = payload;
-
-        const { firstChild } = node
-        const obj = new Component(data)
-
-        if (firstChild) {
-          mount(node, obj, firstChild)
-          node.removeChild(firstChild)
-        }
-        mount(node, obj);
+        mountComponent(Component, node, data);
       });
     }
 
